4b87ad88e02e5f282cbcfbc84aa96ca466657c64
abbie0216 <abbie0216@hotmail.com>
2017-03-23 15:17:43 +0800

    finish

--- app/src/main/java/abbie/chatlistui/MainActivity.java
+++ app/src/main/java/abbie/chatlistui/MainActivity.java
@@ -3,27 +3,45 @@ package abbie.chatlistui;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.list)
     RecyclerView list;
-    ArrayList<ChatItem> items;
+    TextView btnNewMsg;
+    ChatAdapter adapter = new ChatAdapter();
+    ArrayList<ChatItem> items = new ArrayList<>();
+    int numNewMsg = 0;
+    int numTemp = 1;
+
     Handler handler = new Handler();
     Runnable task = new Runnable() {
         @Override
         public void run() {
-            handler.postDelayed(this, 2000);
-            Log.d("Abbie","task run..");
+            handler.postDelayed(this, 1000 * getRandom(1, 5));
+            if (items.size() == 99)
+                items.remove(0);
+            ChatItem item = new ChatItem();
+            item.title = "member";
+            item.content = "chat content num " + (numTemp++);
+            items.add(item);
+            adapter.notifyDataSetChanged();
+            if (!list.canScrollVertically(1)) {
+                Log.d("Abbie", "Get new msg. Already at bottom of list.");
+                list.scrollToPosition(items.size() - 1);
+            } else {
+                Log.d("Abbie", "Get new msg. Not at bottom of list.");
+                btnNewMsg.setText((++numNewMsg) + " 則新訊息");
+                btnNewMsg.setVisibility(View.VISIBLE);
+            }
         }
     };
 
@@ -31,7 +49,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        list = (RecyclerView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        list.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) { //停止滾動
+                    if (recyclerView.canScrollVertically(1)) {
+                        Log.d("Abbie", "Not at bottom of list.");
+                    } else {
+                        Log.d("Abbie", "Already at bottom of list.");
+                        btnNewMsg.setVisibility(View.INVISIBLE);
+                        numNewMsg = 0;
+                    }
+                }
+            }
+        });
+        btnNewMsg = (TextView) findViewById(R.id.btn_new_msg);
+        btnNewMsg.setOnClickListener(this);
     }
 
     @Override
@@ -46,33 +87,55 @@ public class MainActivity extends AppCompatActivity {
         handler.removeCallbacks(task);
     }
 
-    class ChatViewHolder extends RecyclerView.ViewHolder{
-
-        public ChatViewHolder(View itemView) {
-            super(itemView);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_new_msg:
+                list.scrollToPosition(items.size() - 1);
+                btnNewMsg.setVisibility(View.INVISIBLE);
+                numNewMsg = 0;
+                break;
         }
     }
 
-    class ChatAdapter extends RecyclerView.Adapter<ChatViewHolder>{
+    private class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
 
         @Override
         public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ChatViewHolder(getLayoutInflater().inflate(R.layout.view_chat_item,parent));
+            return new ChatViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_chat_item, parent, false));
         }
 
         @Override
         public void onBindViewHolder(ChatViewHolder holder, int position) {
-
+            ChatItem item = items.get(position);
+            holder.txtTitle.setText(item.title);
+            holder.txtContent.setText(item.content);
         }
 
         @Override
         public int getItemCount() {
             return items.size();
         }
+
+        public class ChatViewHolder extends RecyclerView.ViewHolder {
+
+            TextView txtTitle;
+            TextView txtContent;
+
+            public ChatViewHolder(View itemView) {
+                super(itemView);
+                txtTitle = (TextView) itemView.findViewById(R.id.txt_title);
+                txtContent = (TextView) itemView.findViewById(R.id.txt_content);
+            }
+        }
     }
 
-    class ChatItem{
-        int type;
+    private class ChatItem {
+        String title;
+        String content;
+    }
 
+    private int getRandom(int min, int max) {
+        return (int) (Math.floor(Math.random() * (max - min + 1) + min));
     }
 }
